c1d36e390745010488073640eb51345e3388bf1f
Slieo <slimanmad1@gmail.com>
2019-10-13 01:36:08 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -236,21 +236,6 @@ var swiper = new Swiper('.news-slider', {
 		k+=1
 	})
 
-let team = document.getElementById("team-inf");
-let Hscreen = document.getElementsByClassName("hover-screen")[0];
-console.dir(Hscreen);
-document.addEventListener("click", element =>{
-      console.dir(element)
-    if(element.target.className.includes("hover-screen")){
-        team.style.transform = "translate(0, 0%) "
-    }
- if(element.target.id === "close"){
-   
-    team.style.transform = "translate(0, 100%) "
-   
- } 
-})
-
 })
 
 
